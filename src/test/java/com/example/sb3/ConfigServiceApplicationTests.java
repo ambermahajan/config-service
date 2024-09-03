9b7a3a8aa8c112c5bfddce1aaeef4d1d672656af
Amber Mahajan <amber.mahajan@on24.com>
2024-09-03 12:23:19 +0530

    Customized the git path and ssh keys

--- src/test/java/com/example/sb3/ConfigServiceApplicationTests.java
+++ src/test/java/com/example/sb3/ConfigServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sb3ApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
